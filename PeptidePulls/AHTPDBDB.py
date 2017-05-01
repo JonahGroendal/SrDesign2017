76163a6c8daa3f1684872a852174389dbeea14e8
Angelo Danducci <Angelo.Danducci@wmich.edu>
2017-05-01 13:28:55 -0400

    need to work on canonical form, updating existing peptides

---
+++ PeptidePulls/AHTPDBDB.py
@@ -0,0 +1,55 @@
+## separate directories for each website to avoid global var issues
+##
+## are there database lookups for canonical sequence names
+## Sequence, name, length, source, "reactions"?, fereferences, type
+## true or false per field
+##
+## raw dump - careful of comments
+## check sequence length actually matches
+##
+
+from pymongo import MongoClient
+import codecs
+import os
+
+client = MongoClient()
+db = client.test
+f = open(os.path.dirname(os.path.realpath(__file__)) + '/downloads/AHTPDB.txt')
+
+#f = open("aonline.csv",encoding='utf8')
+i = 0
+for line in f:
+    input = line
+    input = input.replace('\n','')
+    input = input.replace('"','')
+    input = input.split("\t")
+    #print(input[1] + "    " + input[2])
+
+    if(1):
+        pass
+    else:
+        try:
+            post =  {
+                    "assay":input[8],
+                    "bitter":input[9],
+                    "pi":input[10],
+                    "bp":input[11],
+                    "method":input[7],
+                    "mice":input[6],
+                    "source":input[5],
+                    "ic50":input[4],
+                    "molecular weight":input[3],
+                    "seqlen":input[2],
+                    "sequence":input[1],
+                    "peptide source":"http://crdd.osdd.net/raghava/ahtpdb/downloads/pepic50.txt",
+                    "_id":input[1] #sequence
+            }
+            result = db.test3.insert_one(post)
+        except:
+            print("key " + input[1] + " already in db")
+    i = i + 1
+
+#collection = db.test9
+#cursor = collection.find({})
+#for u in cursor:
+#    print(u)
--- PeptidePulls/WALTZDB.py
+++ PeptidePulls/WALTZDB.py
@@ -14,7 +14,7 @@ import os
 
 client = MongoClient()
 db = client.test
-f = open(os.path.dirname(os.path.realpath(__file__)) + '/downloads/aonline.csv')
+f = open(os.path.dirname(os.path.realpath(__file__)) + '/downloads/waltzdb_export.csv')
 
 #f = open("aonline.csv",encoding='utf8')
 i = 0
@@ -25,25 +25,25 @@ for line in f:
     input = input.replace('"','')
     input = input.split(",")
 
-    if(input[8].isdigit()):
+    if(input[4].isdigit()):
         pass
     else:
         try:
             post =  {
-                    "type":input[1],
-                    "description":input[2],
-                    "form":input[4],
-                    "toxicity":input[3],
-                    "HLADQ":input[5],
-                    "refs":input[6],
-                    "seqlen":input[7],
-                    "sequence":input[8],
-                    "peptide source":"http://www.allergenonline.org/celiacbrowse.shtm",
-                    "_id":input[8] #sequence
+                    "classification":input[0],
+                    "subset":input[9],
+                    "WALTZ":input[8],
+                    "TANGO":input[5],
+                    "proteostat binding":input[3],
+                    "seqlen":len(input[4]),
+                    "sequence":input[4],
+                    "peptide source":"http://waltzdb.switchlab.org",
+                    "_id":input[4] #sequence
             }
             result = db.test3.insert_one(post)
         except:
-            print("key " + input[8] + " already in db")
+            print("key " + input[4] + " already in db")
+    print(i)
     i = i + 1
 
 #collection = db.test9
