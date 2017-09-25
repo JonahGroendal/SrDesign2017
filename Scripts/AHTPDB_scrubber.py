3910871c7c5d28bce90f21d8ce5b68ffb5f390e9
hijatha <jake.e.schuurmans@gmail.com>
2017-09-25 17:48:13 -0400

    2 Scripts Added, scrubbing aonline.txt and AHTPDB.txt

---
+++ Scripts/AHTPDB_scrubber.py
@@ -0,0 +1,13 @@
+source_path = "../PeptidePulls/downloads/AHTPDB.txt"
+source = open(source_path, "r")
+destination_path = "./scrubbed/AHTPDB_scrubbed.csv"
+destination = open(destination_path, "w")
+
+for line in source:
+	line = line.rstrip()
+	tokens = line.split('	')
+
+	#Sequence:		tokens[1]
+	#Source:		tokens[5]
+	#Properties:	NA
+	#Source Data: http://crdd.osdd.net/raghava/ahtpdb/downloads/pepic50.txt
