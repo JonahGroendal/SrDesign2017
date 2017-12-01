5a6f217fe4c64c2b863741d3196543b7a783ab18
hijatha <jacob.e.schuurmans@wmich.edu>
2017-12-01 15:50:22 -0500

    Connected MongoDB to PHP successfully, now how to read it correctly?, Also updated Setup.sh appropiately for MongoDB PHP driver

--- Setup.sh
+++ Setup.sh
@@ -6,7 +6,7 @@
 sudo apt-get update
 
 #LAMP: Ubuntu 16.04, Apache2, MongoDB, PHP7
-sudo apt-get install apache2 libapache2-mod-php7.0 mongodb -y
+sudo apt-get install apache2 php7.0 libapache2-mod-php7.0 mongodb -y
 
 #Python3 (python3 should be installed by default on Ubuntu, this is precautionary)
 sudo apt-get install python3 -y
@@ -22,9 +22,9 @@ sudo pip3 install beautifulsoup4
 sudo pip3 install requests
 
 #Install PHP MongoDB Driver
-#First install PECL
-sudo apt-get install php-pear php7.0-dev libcurl3-openssl-dev -y
+
+sudo apt-get install php-pear php7.0-dev libcurl4-openssl-dev -y
 #Install the Driver
 sudo pecl install mongodb
 
-echo "\n***********************************************\n*ADD \"extensions=mongodb.so\" to \"php.ini\"     *\n*Type \"php --ini\" to find the files location. *\n***********************************************"
+echo "\n*******************************************************\n*ADD \"extensions=mongodb.so\" to \"apache2/php.ini\"     *\n*Type \"php --ini\" to find the files location. *\n*******************************************************"
--- site/php/database.php
+++ site/php/database.php
@@ -23,28 +23,36 @@
 	<div id="table_div" class="tablepage form-inline no-footer">
 		<table id="peptide_table" class="table table-bordered">
 		  <?php
-          // phpinfo();
-            // require '../vendor/autoload.php';
-            //
-            // $db = new MongoDB\Client("peptide://localhost:27017");
-            //
-            // // select a collection (analogous to a relational database's table)
-            // $collection = $db->peptide;
-            //
-			// // find everything in the collection
+            require '../vendor/autoload.php';
+
+			// $db = $m->local;				// select a database
+			// $collection = $db->directors;  // select a collection (table in old DB)
+			$db = new MongoDB\Client("mongodb://localhost:27017");
+			$collection = $db->peptide->peptide;
+			$cursor = $collection->find(); // view all
+			foreach ($cursor as $doc)
+				{
+    				var_dump($doc);
+				}
+			// $db = new MongoDB\Client("mongodb://localhost:27017");
+
+            // select a collection (analogous to a relational database's table)
+            // $collection = $db->peptide->peptide;
+
+			// find everything in the collection
 			// $cursor = $collection->find();
-
-		  	echo '<script> document.getElementById("table_div").style.visibility = "hidden"; </script>';
-
-			$myfile = fopen("../res/db_dump.csv", "r") or die("Unable to open file!");
-
-			$line = fgets($myfile);
-			$tokens = explode("|", $line);
-
-			echo "<thead><tr>";
-
-			$shrink_count = 0;
-
+            //
+		  	// echo '<script> document.getElementById("table_div").style.visibility = "hidden"; </script>';
+            //
+			// $myfile = fopen("../res/db_dump.csv", "r") or die("Unable to open file!");
+            //
+			// $line = fgets($myfile);
+			// $tokens = explode("|", $line);
+            //
+			// echo "<thead><tr>";
+            //
+			// $shrink_count = 0;
+            //
 			// for ($i = 0; $i < count($tokens); $i++)
 			// {
 			// 	if ($shrink_count < 3) //Change 3 to change the number of Columns keep their name
@@ -63,54 +71,55 @@
 			// 		'\')">?</th>';
 			// 	}
 			// }
-            foreach ($cursor as $document)
-            {
-                foreach ($document as $attribute)
-                {
-                    if ($shrink_count < 3) //Change 3 to change the number of Columns keep their name
-        				{
-        					echo '<th>' . $attribute . '</th>';
-        					$shrink_count++;
-        				}
-        				else
-        				{
-        					echo '<th id = "' .
-        					$attribute .
-        					'" onmouseover="this.innerHTML=\'' .
-        					$attribute .
-        					'\';" onmouseout="this.innerHTML=\'?\';" onclick="click_on(\'' .
-        					$attribute .
-        					'\')">?</th>';
-        				}
-                }
-            }
-
-			echo "</tr></thead><tbody>";
-
-			while (!feof($myfile))
-			{
-				$line = fgets($myfile);
-				$tokens = explode("|", $line);
+			// echo "</tr></thead><tbody>";
+			// $shrink_count = 0;
+            // foreach ($cursor as $document)
+            // {
+            //     foreach ($document as $attribute)
+            //     {
+            //         if ($shrink_count < 3) //Change 3 to change the number of Columns keep their name
+        	// 			{
+        	// 				echo '<th>' . "$attribute" . '</th>';
+        	// 				$shrink_count++;
+        	// 			}
+        	// 			else
+        	// 			{
+        	// 				echo '<th id = "' .
+        	// 				$attribute .
+        	// 				'" onmouseover="this.innerHTML=\'' .
+        	// 				$attribute .
+        	// 				'\';" onmouseout="this.innerHTML=\'?\';" onclick="click_on(\'' .
+        	// 				$attribute .
+        	// 				'\')">?</th>';
+        	// 			}
+            //     }
+            // }
 
 
-				if (strlen(trim($tokens[0])) < 50)
-				{
-					echo "<tr>";
-					for ($i = 0; $i < count($tokens); $i++)
-					{
-						if (trim($tokens[$i]) === "None")
-						{
-								$tokens[$i] = "NA";
-						}
-						echo "<td>" . trim($tokens[$i]) . "</td>";
-					}
-					echo "</tr>";
-				}
-			}
+			// while (!feof($myfile))
+			// {
+			// 	$line = fgets($myfile);
+			// 	$tokens = explode("|", $line);
+            //
+            //
+			// 	if (strlen(trim($tokens[0])) < 50)
+			// 	{
+			// 		echo "<tr>";
+			// 		for ($i = 0; $i < count($tokens); $i++)
+			// 		{
+			// 			if (trim($tokens[$i]) === "None")
+			// 			{
+			// 					$tokens[$i] = "NA";
+			// 			}
+			// 			echo "<td>" . trim($tokens[$i]) . "</td>";
+			// 		}
+			// 		echo "</tr>";
+			// 	}
+			// }
 
-			echo "</tbody>";
+			// echo "</tbody>";
 
-			fclose($myfile);
+			// fclose($myfile);
 
 			echo '<script> document.getElementById("table_div").style.visibility = "visible"; </script>';
 		   ?>
