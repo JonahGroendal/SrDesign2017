99ffb3a18e630ee3dd0678d4c252c4f200534329
hijatha <jake.e.schuurmans@gmail.com>
2017-12-04 18:39:26 -0500

    Changed extensions=mongodb.so to enxtension=mongodb.so

--- Setup.sh
+++ Setup.sh
@@ -6,7 +6,13 @@
 sudo apt-get update
 
 #LAMP: Ubuntu 16.04, Apache2, MongoDB, PHP7
-sudo apt-get install apache2 php7.0 libapache2-mod-php7.0 mongodb -y
+sudo apt-get install apache2 php7.0 libapache2-mod-php7.0 -y
+
+#MongoDB
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
 
 #Python3 (python3 should be installed by default on Ubuntu, this is precautionary)
 sudo apt-get install python3 -y
@@ -27,4 +33,6 @@ sudo apt-get install php-pear php7.0-dev libcurl4-openssl-dev -y
 #Install the Driver
 sudo pecl install mongodb
 
-echo "\n*******************************************************\n*ADD \"extension=mongodb.so\" to \"apache2/php.ini\"     *\n*Type \"php --ini\" to find the files location. *\n*******************************************************"
+sudo bash -c "echo 'extension=mongodb.so' >> /etc/php/7.0/apache2/php.ini"
+echo "*****ATTENTION******\nVerify the root directory of Apache2 is that of SrDesign2017/site/";
+# echo "\n*******************************************************\n*ADD \"extension=mongodb.so\" to \"apache2/php.ini\"     *\n*Type \"php --ini\" to find the files location. *\n*******************************************************"
