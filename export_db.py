5c68b7a3006dfb851ff77900ac83d2c0104a995c
jakkabbi <mcclure.jack.e@gmail.com>
2017-12-11 17:10:59 -0500

    changed destination for dump to site/res

--- export_db.py
+++ export_db.py
@@ -5,4 +5,4 @@ import db_schema
 
 d = db.PeptideDB()
 fields_to_print = [k for k in db_schema.peptide_db_schema["peptide"]["_for_each"]["_schema"] if k != "source"]
-d.export_peptides_to_csv("db_dump.csv", fields_to_print, pretty=True)
+d.export_peptides_to_csv("site/res/db_dump.csv", fields_to_print, pretty=True)
